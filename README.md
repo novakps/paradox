05153ecc8ac1f186e34a71f853727e0d53610707
Paul Novak <paul.novak@schrodinger.com>
2016-05-13 09:37:37 -0400

    setup instructions.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # paradox
 fake docking service.  Returns a 2D and 3D pair-of-docks.
+
+How to run:
+
+have rdkit installed. For OSX:
+
+    brew update
+    brew tap rdkit/rdkit
+    brew install rdkit
+
+checkout this repo and create a virtualenv
+
+    virtualenv env
+
+add the following two line to env/bin/activate to add rdkit to python path
+
+    # rdkit environment vars
+    export RDBASE=/opt/boxen/homebrew/share/RDKit
+    export PYTHONPATH=/opt/boxen/homebrew/lib/python2.7/site-packages:$PYTHONPATH
+
+activate the virtualenv
+
+    source env/bin/activate
+
+install deps
+
+    pip install -r requirements.txt
+
+start the server
+
+    python paradox.py
---
+++ paradox.py
@@ -0,0 +1,66 @@
+'''
+paradox
+fake docking api using flask redkit and pymol xmlrpc
+
+
+'''
+from flask import Flask, jsonify, request
+import base64
+import os
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import subprocess
+from tempfile import NamedTemporaryFile
+import xmlrpclib
+
+app = Flask(__name__)
+proc = subprocess.Popen(['/Applications/MacPyMOL.app/Contents/MacOS/MacPyMOL', '-R'])
+pymol = xmlrpclib.Server('http://localhost:9123/RPC2')
+
+@app.route('/dock', methods=['POST'])
+def dock():
+    ''' POST json containing a required 2D molfile string and an optional 3D molfile string.
+     - Get back json containing fake-docked pse and 2D molfile aligned to original 2D that is derived from 3D--if present--or from orignal 2D. '''
+    content = request.get_json(force=True)
+    mol2d = content['2D']
+    mol3d = content.get('3D')
+    if mol2d:
+        # always used as alignment template, also used as source if no 3D provided
+        template = Chem.MolFromMolBlock(mol2d)
+    if mol3d:
+        # use 3D as source, if present
+        source = Chem.MolFromMolBlock(mol3d)
+    else:
+        # otherwise use 2D as source
+        source = template
+
+    # convert to 3D and write to a temp file
+    molecule = Chem.AddHs(source)
+    AllChem.EmbedMolecule(molecule)
+    AllChem.UFFOptimizeMolecule(molecule)
+    f = NamedTemporaryFile(delete=False, suffix='.mol')
+    writer = Chem.SDWriter(f)
+    writer.write(molecule)
+    writer.flush()
+    f.close()
+
+    # conversion to pse
+    pymol.reinitialize()
+    pymol.load(f.name)
+    pymol.show('sticks')
+    pse_name = f.name + '.pse'
+    pymol.save(pse_name)
+    with open(pse_name) as f_pse:
+        encoded = base64.b64encode(f_pse.read())
+    os.unlink(f.name)
+    os.unlink(pse_name)
+
+    # back to 2D and align with template
+    AllChem.Compute2DCoords(molecule)
+    AllChem.GenerateDepictionMatching2DStructure(molecule, template)
+    result = {'2D': Chem.MolToMolBlock(molecule), 'base64pse': encoded }
+    return jsonify(result);
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    proc.communicate()[0]
